0e8c9c4b749901636a39e50a1eb69592c4367aa1
0xtanapat <99159162+0xtanapat@users.noreply.github.com>
2024-01-28 18:08:00 +0700

    Create page views and route urls to those pages

--- general/views.py
+++ general/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# def ecommerce_index_view(request):
+#     '''This function render index page of ecommerce views'''
+#     return HttpResponse('Welcome to 6410742388 Tanapat Dejchodanan views!')
+
+# def item_view(request, item_id):
+#     context_data = {
+#         "item_id": item_id
+#     }
+#     return render(request, 'index.html', context = context_data)
+
+def homepage(request):
+    return HttpResponse('Homepage')
+
+def category(request):
+    return HttpResponse('Category page')
+
+def product(request):
+    return HttpResponse('Product page')
+
+def checkout(request):
+    return HttpResponse('Checkout page')
+
+def contact(request):
+    return HttpResponse('Contact page')
